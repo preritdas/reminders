a034287e5840aba33ded9d3446e8672961d5b421
anishch <43155397+anishch@users.noreply.github.com>
2022-02-01 10:59:22 -0800

    more documentations + changed one line, for better coding practices

--- mainpd.py
+++ mainpd.py
@@ -5,9 +5,9 @@ from texts import *
 import os
 
 if __name__ == "__main__":
-    print("Reminders bot is ready to go.")
+    print("Reminders bot is ready to go.") # wake-up
     while True:
-        # Update Data
+        # Update Data (Reading all files in - File/IO)
         try:
             remindersDF = pd.read_json("Data/reminders.json")
             reminders = list(remindersDF["Reminder"])
@@ -19,21 +19,22 @@ if __name__ == "__main__":
             while os.path.getmtime('Data/reminders.csv') == modified:
                 time.sleep(1)
 
-        if len(reminders) != len(times):
+        if len(reminders) != len(times): # our csv is mismatched, so possibility of null return is caught
             print("Reminders aren't corresponding with times.")
             textMe("Reminders aren't corresponding with times. Please correct.")
-            modified = os.path.getmtime('Data/reminders.csv')
+            modified = os.path.getmtime('Data/reminders.csv') # since the file won't change in loop, we call it again. if it isn't fixed, it will be stuck.
             while os.path.getmtime('Data/reminders.csv'):
                 time.sleep(1)
-        
-        currentTime = dt.now().strftime("%H-%M")
+
+        currentTime = dt.now().strftime("%H-%M") # [AC, questions on how often this iterates.]
         if currentTime in times:
             for i in range(len(times)):
                 if times[i] == currentTime:
                     break
                 else:
                     pass
-            response = textMe(reminders[i])
+            message = reminders([i]) # [AC, changes made for clarity + easier debugging]
+            response = textMe(message) # [AC, saving as local variable, then sending]
             print(f"Successfully reminded {reminders[i]}" if response == '0' else f"message failure at {dt.now()}")
             time.sleep(60)
         else:
